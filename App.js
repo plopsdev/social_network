22cd32d8642f7020a29db2d1319ab4f7cd648c95
plopsdev <jonathan.miel@outlook.com>
2021-04-26 16:53:25 +0200

    icons

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 
 import Feed from './screens/Feed'
 import CreatePost from './screens/CreatePost'
@@ -18,12 +21,32 @@ function App() {
 
     return(
         <NavigationContainer>
-            <Tab.Navigator initialRouteName='Home'>
-                <Tab.Screen name='Feed' component ={Feed}/>
-                <Tab.Screen name='Notifications' component ={Notifications}/>
-                <Tab.Screen name='Add' component={CreatePost}/>
-                <Tab.Screen name='Profile' component={Profile}/>
-                <Tab.Screen name='Research' component={Research}/>
+            <Tab.Navigator initialRouteName='Feed'>
+                <Tab.Screen name='Feed' component={Feed} options={{
+                    tabBarIcon: ({color}) => (
+                        <Icon name='home' size={25} color={color}/>
+                    )
+                }}/>
+                <Tab.Screen name='Notifications' component ={Notifications} options={{
+                    tabBarIcon: ({color}) => (
+                        <Icon name='bell' size={25} color={color}/>
+                    )
+                }}/>
+                <Tab.Screen name='Add' component={CreatePost} options={{
+                    tabBarIcon: ({color}) => (
+                        <Icon name='plus' size={25} color={color}/>
+                    )
+                }}/>
+                <Tab.Screen name='Profile' component={Profile} options={{
+                    tabBarIcon: ({color}) => (
+                        <Icon name='user' size={25} color={color}/>
+                    )
+                }}/>
+                <Tab.Screen name='Research' component={Research} options={{
+                    tabBarIcon: ({color}) => (
+                        <Icon name='search' size={25} color={color}/>
+                    )
+                }}/>
             </Tab.Navigator>
         </NavigationContainer>
     )
