675a00b1be9b15e426c3d4a1fc93c8682d1aea1c
plopsdev <jonathan.miel@outlook.com>
2021-04-26 19:38:14 +0200

    context

--- App.js
+++ App.js
@@ -14,51 +14,49 @@ import Profile from './screens/Profile'
 import Research from './screens/Research'
 import SignUp from './screens/SignUp'
 import User from './screens/User'
- 
+import fakeData from './FakeData'
+import { DataContext } from './services/dataContext'
+
+
 
 function App() {
     const Tab = createBottomTabNavigator();
 
     return(
-        <NavigationContainer>
-            <Tab.Navigator initialRouteName='Feed'>
-                <Tab.Screen name='Feed' component={Feed} options={{
-                    tabBarIcon: ({color}) => (
-                        <Icon name='home' size={25} color={color}/>
-                    )
-                }}/>
-                <Tab.Screen name='Notifications' component ={Notifications} options={{
-                    tabBarIcon: ({color}) => (
-                        <Icon name='bell' size={25} color={color}/>
-                    )
-                }}/>
-                <Tab.Screen name='Add' component={CreatePost} options={{
-                    tabBarIcon: ({color}) => (
-                        <Icon name='plus' size={25} color={color}/>
-                    )
-                }}/>
-                <Tab.Screen name='Profile' component={Profile} options={{
-                    tabBarIcon: ({color}) => (
-                        <Icon name='user' size={25} color={color}/>
-                    )
-                }}/>
-                <Tab.Screen name='Research' component={Research} options={{
-                    tabBarIcon: ({color}) => (
-                        <Icon name='search' size={25} color={color}/>
-                    )
-                }}/>
-            </Tab.Navigator>
-        </NavigationContainer>
+        <DataContext.Provider value = {{
+            posts: fakeData
+        }}>
+            <NavigationContainer>
+                <Tab.Navigator initialRouteName='Feed'>
+                    <Tab.Screen name='Feed' component={Feed} options={{
+                        tabBarIcon: ({color}) => (
+                            <Icon name='home' size={25} color={color}/>
+                        )
+                    }}/>
+                    <Tab.Screen name='Notifications' component ={Notifications} options={{
+                        tabBarIcon: ({color}) => (
+                            <Icon name='bell' size={25} color={color}/>
+                        )
+                    }}/>
+                    <Tab.Screen name='Add' component={CreatePost} options={{
+                        tabBarIcon: ({color}) => (
+                            <Icon name='plus' size={25} color={color}/>
+                        )
+                    }}/>
+                    <Tab.Screen name='Profile' component={Profile} options={{
+                        tabBarIcon: ({color}) => (
+                            <Icon name='user' size={25} color={color}/>
+                        )
+                    }}/>
+                    <Tab.Screen name='Research' component={Research} options={{
+                        tabBarIcon: ({color}) => (
+                            <Icon name='search' size={25} color={color}/>
+                        )
+                    }}/>
+                </Tab.Navigator>
+            </NavigationContainer>
+        </DataContext.Provider>
     )
 }
-
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
-  }
-});
 
 export default App;
